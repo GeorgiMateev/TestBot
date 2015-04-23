d0325af65f327fd7d4a5e8bd104baf39d1f18929
Georgi Mateev <gmmateev@gmail.com>
2015-04-23 08:53:08 +0300

    Added java project containing the data layer

---
+++ testbot.data/src/main/java/testbot/data/App.java
@@ -0,0 +1,15 @@
+package testbot.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.print("Db start");
+        Database db = new Database();
+        db.addDummyData();
+    }
+}
