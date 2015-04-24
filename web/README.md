18ab2fb624efb94905033bd04ad331585279c8e2
Krasimir Kirilov <kkirilov91@gmail.com>
2015-04-25 01:46:44 +0300

    hange struture to be in Spring Rest

---
+++ web/README.md
@@ -0,0 +1,3 @@
+1. Go to /web folder.
+2. Run mvn package
+3. Run java -jar target/TestBot-0.1.0.jar --server.port=[EMPTY_PORT]
--- web/src/main/java/hello/Application.java
+++ web/src/main/java/hello/Application.java
@@ -1,12 +1,12 @@
-package webapp;
+package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestbotApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestbotApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
