26296bb6df5ee772bf4514f6e7e45f551720e83b
Krasimir Kirilov <kkirilov91@gmail.com>
2015-04-25 22:09:01 +0300

    portal styles added

--- web/src/main/resources/static/app.js
+++ web/src/main/resources/static/app.js
@@ -1,3 +1,24 @@
 (function () {
-    console.log('2qlqlql');
-})();
+
+  var startTesting = function() {
+    var loadingIndicator = $('.loading-indicator');
+    var testingButton = $('button.start-testing');
+    var testingPromise = $.ajax('http://localhost:8182/automate');
+    testingButton.hide();
+    loadingIndicator.show();
+
+    testingPromise
+    .then(function() {
+      $('.tests-result').attr('data-available', 'true');
+    })
+    .always(function() {
+      loadingIndicator.hide();
+      testingButton.show();
+    });
+  }
+
+  $(document).ready(function() {
+    var testingButton = $('button.start-testing');
+    testingButton.click(startTesting);
+  });
+}());
