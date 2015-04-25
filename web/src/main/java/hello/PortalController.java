3941d50b5d32431ae2aebf6a5f7081660f61cfae
Krasimir Kirilov <kkirilov91@gmail.com>
2015-04-26 02:11:33 +0300

    db service for retrieving run results added and connected to portal

--- web/src/main/java/hello/PortalController.java
+++ web/src/main/java/hello/PortalController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PortalController {
@@ -19,31 +20,7 @@ public class PortalController {
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         ArrayList<IssueDiff> list = new ArrayList<>();
 
-        IssueDiff m = new IssueDiff();
-        m.setActualParentHtml("<ul><li></li></ul>");
-        m.setExpectedParentHtml("<ul><li></li><li></li></ul>");
-        IssueDiff m1 = new IssueDiff();
-        m1.setActualParentHtml("<ul><li></li></ul>");
-        m1.setExpectedParentHtml("<ul><li></li><li></li></ul>");
-        IssueDiff m2 = new IssueDiff();
-        m2.setExpectedParentHtml("<ul><li></li><li></li></ul>");
-        m2.setActualParentHtml("<ul><li></li></ul>");
-        IssueDiff m3 = new IssueDiff();
-        m3.setExpectedParentHtml("<ul><li></li><li></li></ul>");
-        m3.setActualParentHtml("<ul><li></li></ul>");
-        list.add(m);
-        list.add(m1);
-        list.add(m2);
-        list.add(m3);
-
-        RunResult run = new RunResult();
-        run.setIssues(list);
-
-        RunResult run2 = new RunResult();
-        run2.setIssues(list);
-        ArrayList<RunResult> runs = new ArrayList<>();
-        runs.add(run);
-        runs.add(run2);
+        List<RunResult> runs = db.getResults();
         model.addAttribute("runs", runs);
         return "portal";
     }
